a995edc34b60b82d400541d8543a9e3947f9cd29
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2022-02-04 09:44:39 +0200

    Better Printer

--- Task2/MapPrinter.cs
+++ Task2/MapPrinter.cs
@@ -1,17 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Task2
 {
     public class MapPrinter
     {
-        public void Print(string[,] maze)
+        public void Print(string[,] maze, List<Point> pathedMaze)
         {
             PrintTopLine();
+            for (var pathedI = 1; pathedI <= pathedMaze.Count - 1; pathedI++)
+            {
+                var newX = pathedMaze[pathedI].Column;
+                var newY = pathedMaze[pathedI].Row;
+                maze[newX, newY] = "*";
+            }
+
+            // maze[pathedMaze[0].Column, pathedMaze[0].Row] = "A";
+            // maze[pathedMaze[pathedMaze.Count - 1].Column, pathedMaze[pathedMaze.Count - 1].Row] = "B";
             for (var row = 0; row < maze.GetLength(1); row++)
             {
                 Console.Write($"{row}\t");
-                for (var column = 0; column < maze.GetLength(0); column++) Console.Write(maze[column, row]);
+                for (var column = 0; column < maze.GetLength(0); column++)
+                {
+                    Console.Write(maze[column, row]);
+                }
 
                 Console.WriteLine();
             }
